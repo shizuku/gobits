7e24eff0e117efeeff1102b153347a20ed299445
shizuku <2112165916@qq.com>
2020-11-11 23:25:29 +0800

    feat: finish

--- bits.go
+++ bits.go
@@ -1,12 +1,15 @@
 package gobits
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"strings"
 )
 
 type Bits struct {
-	data []byte
-	len  int
+	data   []byte
+	offset int
 }
 
 func New() *Bits {
@@ -14,37 +17,112 @@ func New() *Bits {
 }
 func FromBytes(bys []byte) *Bits {
 	return &Bits{
-		data: bys,
-		len:  len(bys) * 8,
+		data:   bys,
+		offset: 0,
 	}
 }
-func FromByte(by byte, nn int) *Bits {
+func FromByte(by byte, offset int) *Bits {
+	if offset < 0 || offset >= 8 {
+		log.Fatal(errors.New("offset out of range."))
+	}
 	return &Bits{
-		data: []byte{by & (0xFF >> (8 - nn))},
-		len:  nn,
+		data:   []byte{by & (0xFF >> (8 - offset))},
+		offset: offset,
 	}
 }
 func (b *Bits) Len() int {
-	return b.len
+	return len(b.data)*8 - (8 - b.offset)
 }
-func (b *Bits) AppendBits(bts *Bits) error {
+func (b *Bits) Append(bts *Bits) error {
+	l := len(bts.data)
+	for i, v := range bts.data {
+		if i == l-1 {
+			b.AppendBits(v, bts.offset)
+		} else {
+			b.AppendByte(v)
+		}
+	}
 	return nil
 }
-func (b *Bits) AppendByte(by byte) error {
-	if b.len%8 == 0 {
-		b.data = append(b.data, by)
-		b.len = b.len + 8
+func (b *Bits) AppendBits(by byte, offset int) error {
+	if offset < 0 || offset >= 8 {
+		return errors.New("offset out of range.")
+	}
+	
+	return nil
+}
+func (b *Bits) AppendBit(by byte) {
+	ap := (by >> (8 - b.offset)) | ((by << b.offset) & (0xff >> (8 - b.offset - 1)))
+	//fmt.Printf("debug: %d, %#08b\n", b.offset, ap)
+	if b.offset == 0 {
+		b.data = append(b.data, ap)
 	} else {
-
+		b.data[len(b.data)-1] = b.data[len(b.data)-1] | ap
+	}
+	b.offset = b.offset + 1
+	if b.offset >= 8 {
+		b.offset = 0
+	}
+}
+func (b *Bits) AppendByte(by byte) error {
+	if b.offset == 0 {
+		b.data = append(b.data, by)
+	} else {
+		ty := (b.data[len(b.data)-1] & (0xff >> b.offset)) | (by & (0xff << (8 - b.offset)))
+		ny := by & (0xff >> (8 - b.offset))
+		b.data = append(b.data[:len(b.data)-1], ty)
+		b.data = append(b.data, ny)
 	}
 	return nil
 }
 func (b *Bits) Bytes() []byte {
 	return b.data
 }
-func (b *Bits) Print() {
-	fmt.Print("0b")
-	for _, v := range b.data {
-		fmt.Printf("%b_", v)
+func (b *Bits) String() string {
+	var s strings.Builder
+	l := len(b.data)
+	for i, v := range b.data {
+		if i == l-1 {
+			d := 8
+			if b.offset != 0 {
+				d = b.offset
+			}
+			s.WriteString(fmt.Sprintf("%0"+fmt.Sprint(d)+"b", v))
+		} else {
+			s.WriteString(fmt.Sprintf("%08b_", v))
+		}
 	}
+	return s.String()
+}
+func (b *Bits) Itor() *Iterator {
+	return &Iterator{bts: b, idx: 0, offset: 7}
+}
+
+type Iterator struct {
+	bts    *Bits
+	idx    int
+	offset int
+}
+
+func (it *Iterator) Next() (by byte, idx int, err error) {
+	if it.idx > (len(it.bts.data)-1) || (it.idx == (len(it.bts.data)-1) && it.offset < it.bts.offset) {
+		by = 0
+		idx = 0
+		err = errors.New("end of iterator.")
+		return
+	}
+	x := it.bts.data[it.idx]
+	if it.idx == (len(it.bts.data) - 1) {
+		by = ((x >> (it.offset - it.bts.offset)) & 0x01)
+	} else {
+		by = ((x >> it.offset) & 0x01)
+	}
+	idx = it.idx*8 + (7 - it.offset)
+	err = nil
+	it.offset = it.offset - 1
+	if it.offset < 0 {
+		it.idx = it.idx + 1
+		it.offset = 7
+	}
+	return
 }
--- example/main.go
+++ example/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/shizuku/gobits"
 )
 
 func main() {
-	bits := gobits.FromByte(0xff, 8)
-	bits.Print()
+	x0 := gobits.FromByte(0, 1)
+	println(x0.String())
+	x10 := gobits.FromByte(2, 2)
+	println(x10.String())
+	x110 := gobits.FromByte(6, 3)
+	println(x110.String())
+	x1110 := gobits.FromByte(14, 4)
+	println(x1110.String())
+}
 
-	// by := 0xff
-	// fmt.Printf("%#b\n", (by & (0xff >> 4)))
-	// fmt.Printf("%#x\n", 0x0f)
+func printByteArray(bts []byte) {
+	fmt.Print("[")
+	for _, v := range bts {
+		fmt.Printf("%x,", v)
+	}
+	fmt.Print("]\n")
 }
